afa9a6d2bf4ffb75bc97e700f21900e303c8ea77
Kun Gábor 4IG <ext-kung@antsz.local>
2026-08-12 13:19:07 +0200

    fix: add host to vite config

--- apps/host-client/vite.config.ts
+++ apps/host-client/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
@@ -12,6 +12,7 @@ export default defineConfig({
     },
   },
   server: {
+    host: true,
     port: 5173,
   },
 });
